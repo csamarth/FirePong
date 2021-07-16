63b82e38fa75dc04e4cbe915b14b9992791ac24e
thebigg <yunior.eury@gmail.com>
2021-07-15 22:23:11 -0500

    -Added MainMenu Scene -Added Current Selection Rect(empty at the moment)

---
+++ MainMenu.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+export(PackedScene) var Game
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func start_game():
+#	Start game
+	pass
+
+
+func exit_game():
+#	Exit game
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
