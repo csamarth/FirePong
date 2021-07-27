b8bae5e11bb71799cc540f5d2303dc68b6c14c6c
Samarth <c.samarth98@gmail.com>
2021-07-26 21:00:33 -0500

    basic working physics

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -5,13 +5,11 @@ var vel = Vector2.ZERO
 var direction = 1.0
 
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	vel.y = direction * 100
 	vel.x = 0
 	vel = move_and_slide(vel, Vector2.UP)
 	if is_on_floor():
-		print("floor")
 		direction *= -1
 	if is_on_ceiling():
-		print("ceiling")
 		direction *= -1
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,6 +3,6 @@ extends Actor
 func _physics_process(_delta):
 	var direction := Vector2( 
 		0.0,	#x-axis
-		Input.get_action_strength("move_down") -Input.get_action_strength("move_up") 	#y-axis
+		Input.get_action_strength("ui_down") -Input.get_action_strength("ui_up") 	#y-axis
 	);
 	_velocity = speed * direction;
