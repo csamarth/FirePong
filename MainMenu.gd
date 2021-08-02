c6069fb05201bbce2c3f98393e8b0b1842034fcc
thebigg <yunior.eury@gmail.com>
2021-08-02 01:37:40 -0700

    -Integrate translation into MainMenu scene.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export(PackedScene) var Game
+export(String, "hi", "en", "es") var locale
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -12,8 +13,17 @@ func center_menu():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	This is for presenting the locale to users in a user-friendly manner such as
+#	"English (United States)".
+#	TranslationServer.set_locale(TranslationServer.get_locale_name("hi_IN"))
+
+	TranslationServer.set_locale(locale)
 	center_menu()
 	get_tree().get_root().connect("size_changed", self ,"center_menu")
+	$MarginContainer/GridContainer/Start.set_text(tr("START"))
+	$MarginContainer/GridContainer/Options.set_text(tr("OPTIONS"))
+	$MarginContainer/GridContainer/Exit.set_text(tr("EXIT"))
+	
 
 
 func start_game():
